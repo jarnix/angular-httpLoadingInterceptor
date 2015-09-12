aa5e0aa5fd841970cde2db0c07e32616498be4af
Julien Ricard <jarnix@gmail.com>
2015-09-12 16:18:13 +0200

    annotate

--- angular-http-loading-interceptor.js
+++ angular-http-loading-interceptor.js
@@ -2,63 +2,47 @@
 
     'use strict';
 
-    angular
-	    .module('httpLoadingInterceptor', [])
+    angular.module('httpLoadingInterceptor', [])
 
-	    .factory(
-		    'httpLoadingInterceptorService',
-		    [
-			    "$q",
-			    "$rootScope",
-			    function($q, $rootScope) {
+    .factory('httpLoadingInterceptorService', [ "$q", "$rootScope", function($q, $rootScope) {
 
-				return {
-				    request : function(config) {
-					// Shows the loader
-					$rootScope
-						.$broadcast("httpLoadingInterceptor:show");
-					return config || $q.when(config)
-				    },
-				    response : function(response) {
-					// Hides the loader
-					$rootScope
-						.$broadcast("httpLoadingInterceptor:hide");
-					return response || $q.when(response);
-				    },
-				    responseError : function(response) {
-					// Hides the loader
-					$rootScope
-						.$broadcast("httpLoadingInterceptor:hide");
-					return $q.reject(response);
-				    }
-				};
-			    } ])
-	    .config(
-		    function($httpProvider) {
-			$httpProvider.interceptors
-				.push('httpLoadingInterceptorService');
-		    })
+	return {
+	    request : function(config) {
+		// Shows the loader
+		$rootScope.$broadcast("httpLoadingInterceptor:show");
+		return config || $q.when(config)
+	    },
+	    response : function(response) {
+		// Hides the loader
+		$rootScope.$broadcast("httpLoadingInterceptor:hide");
+		return response || $q.when(response);
+	    },
+	    responseError : function(response) {
+		// Hides the loader
+		$rootScope.$broadcast("httpLoadingInterceptor:hide");
+		return $q.reject(response);
+	    }
+	};
+    } ]).config([ "$httpProvider", function($httpProvider) {
+	$httpProvider.interceptors.push('httpLoadingInterceptorService');
+    } ])
 
-	    .directive(
-		    "httpLoadingInterceptorSpinner",
-		    function() {
+    .directive("httpLoadingInterceptorSpinner", function() {
 
-			return {
-			    restrict : 'A',
-			    template : '<div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div>',
-			    link : function($scope, element, attrs) {
-				$scope.$on("httpLoadingInterceptor:show",
-					function() {
-					    // console.log('show');
-					    return element.show();
-					});
-				$scope.$on("httpLoadingInterceptor:show",
-					function() {
-					    // console.log('hide');
-					    return element.hide();
-					});
-			    }
-			};
-		    })
+	return {
+	    restrict : 'A',
+	    template : '<div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div>',
+	    link : function($scope, element, attrs) {
+		$scope.$on("httpLoadingInterceptor:show", function() {
+		    // console.log('show');
+		    return element.show();
+		});
+		$scope.$on("httpLoadingInterceptor:show", function() {
+		    // console.log('hide');
+		    return element.hide();
+		});
+	    }
+	};
+    })
 
 })();
